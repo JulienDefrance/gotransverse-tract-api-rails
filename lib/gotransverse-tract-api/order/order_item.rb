abe13a889dd8906a8369b89743bae551acd9947b
Ravikumar Gudipati <rgudipati@SZP-MBP-rgudipati.local>
2015-11-20 16:12:19 -0800

    Added and updated order methods as per TRACT 1.28 API

--- lib/gotransverse-tract-api/order/order_item.rb
+++ lib/gotransverse-tract-api/order/order_item.rb
@@ -46,6 +46,36 @@ module GoTransverseTractApi
         GoTransverseTractApi.get_response_for(self, {service_identifier: service_identifier})
       end
 
+      #
+      # @param {Long} service_eid
+      #
+      def self.find_by_service_eid service_eid
+        GoTransverseTractApi.get_response_for(self, {service_eid: service_eid})
+      end
+
+      #
+      # @param {String} discount_identifier
+      #
+      def self.find_by_discount_identifier discount_identifier
+        GoTransverseTractApi.get_response_for(self, {discount_identifier: discount_identifier})
+      end
+
+      #
+      # @param {Long} eid
+      # @param {Nokogiri::XML::Document} approve
+      #
+      def self.approve eid, approve
+        GoTransverseTractApi.post_request_for(self, {eid: eid}, approve.to_s, "approve")
+      end
+
+      #
+      # @param {Long} eid
+      # @param {Nokogiri::XML::Document} deny
+      #
+      def self.deny eid, deny
+        GoTransverseTractApi.post_request_for(self, {eid: eid}, deny.to_s, "deny")
+      end
+
     end
 
   end
