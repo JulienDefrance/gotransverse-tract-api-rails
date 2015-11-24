4ec0b63e980ac84745e048c9f93128a294646476
Julien DeFrance <jdefrance@smartzip.com>
2015-11-24 11:50:27 -0800

    Better wrapping of API responses

--- lib/gotransverse-tract-api.rb
+++ lib/gotransverse-tract-api.rb
@@ -20,10 +20,16 @@ require "gotransverse-tract-api/general/system_setting"
 require "gotransverse-tract-api/general_ledger/general_ledger"
 require "gotransverse-tract-api/general_ledger/gl_account"
 require "gotransverse-tract-api/general_ledger/gl_account_category"
+require "gotransverse-tract-api/general_ledger/gl_accounting_transaction"
+require "gotransverse-tract-api/general_ledger/gl_adjustment_accounting_transaction"
 require "gotransverse-tract-api/general_ledger/gl_accounting_entry"
 require "gotransverse-tract-api/general_ledger/gl_aggregate_batch"
 require "gotransverse-tract-api/general_ledger/gl_aggregate_entry"
+require "gotransverse-tract-api/general_ledger/gl_invoice_accounting_transaction"
+require "gotransverse-tract-api/general_ledger/gl_payment_accounting_transaction"
+require "gotransverse-tract-api/general_ledger/gl_transaction_batch"
 
+require "gotransverse-tract-api/order/adjustment_category"
 require "gotransverse-tract-api/order/agreement"
 require "gotransverse-tract-api/order/bill_cycle"
 require "gotransverse-tract-api/order/billing_account_category"
@@ -31,6 +37,7 @@ require "gotransverse-tract-api/order/order_item"
 require "gotransverse-tract-api/order/order_item_charge"
 require "gotransverse-tract-api/order/organization"
 require "gotransverse-tract-api/order/party_category"
+require "gotransverse-tract-api/order/payment_term"
 require "gotransverse-tract-api/order/people"
 require "gotransverse-tract-api/order/product_relation"
 require "gotransverse-tract-api/order/renewal_order"
@@ -49,6 +56,7 @@ require "gotransverse-tract-api/product/operation_attribute"
 require "gotransverse-tract-api/product/price_list"
 require "gotransverse-tract-api/product/product_price_range"
 require "gotransverse-tract-api/product/product"
+require "gotransverse-tract-api/product/product_tag"
 require "gotransverse-tract-api/product/product_category"
 require "gotransverse-tract-api/product/product_price"
 require "gotransverse-tract-api/product/one_time_product_price"
@@ -128,7 +136,7 @@ module GoTransverseTractApi
     end
 
     # Unless cached
-    self.call(api_url, api_params, :get)
+    self.call(klass, api_url, api_params, :get)
   end
 
   #
@@ -142,7 +150,7 @@ module GoTransverseTractApi
   def self.post_request_for(klass, api_params={}, request_body, command)
     api_url = GoTransverseTractApi.get_api_url_for(klass)
     api_url = "#{api_url}/#{command}"
-    self.call(api_url, api_params, :post, request_body.to_xml)
+    self.call(klass, api_url, api_params, :post, request_body.to_xml)
   end
 
   #
@@ -154,7 +162,7 @@ module GoTransverseTractApi
   #
   def self.put_request_for(klass, api_params={}, request_body)
     api_url = GoTransverseTractApi.get_api_url_for(klass)
-    self.call(api_url, api_params, :put, request_body.to_xml)
+    self.call(klass, api_url, api_params, :put, request_body.to_xml)
   end
 
   private
@@ -178,12 +186,13 @@ module GoTransverseTractApi
   #
   # self.call
   #
+  # @param {Class} klass
   # @param {String} api_url
   # @param {Hash} api_params (optional)
   # @param {String} method (optional)
   # @param {String} request_body (optional, put/post requests only)
   #
-  def self.call(api_url, api_params={}, method=:get, request_body="")
+  def self.call(klass, api_url, api_params={}, method=:get, request_body="")
 
     # Camelize parameters
     api_params = camelize_keys(api_params)
@@ -200,7 +209,13 @@ module GoTransverseTractApi
         response = http_client.put(api_url, request_body, api_params)
     end
 
-    Nokogiri::XML(response.body.to_s)
+    xml_response = Nokogiri::XML(response.body.to_s)
+
+    klass = klass.to_s.split("::").last
+    hsh = Hash.from_xml(xml_response.to_s)
+
+    hsh = hsh[klass.pluralize.camelize(:lower)][klass.camelize(:lower)] rescue Hash.from_xml(xml_response.to_s)[klass.camelize(:lower)]
+    return hsh
   end
 
   #
