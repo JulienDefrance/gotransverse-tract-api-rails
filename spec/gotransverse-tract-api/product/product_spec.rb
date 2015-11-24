3303bf5f7a211a370a085ae118c80c92e0c36d7c
Ravikumar Gudipati <rgudipati@SZP-MBP-rgudipati.local>
2015-11-24 14:55:11 -0800

    Updated specs for products

--- spec/gotransverse-tract-api/product/product_spec.rb
+++ spec/gotransverse-tract-api/product/product_spec.rb
@@ -7,17 +7,15 @@ module GoTransverseTractApi
 
     context ".find_all" do
       it "returns all products" do
-        response = described_class.find_all
-        num_of_products = response.css('products').children.count
-        expect(num_of_products).to be > 1
+        data = described_class.find_all
+        expect(data.count).to be > 1
       end
     end
 
     context ".find_by_eid" do
       it "returns a product for the given eid" do
-        response = described_class.find_by_eid(50)
-        data = Hash.from_xml(response.to_s)
-        expect(data['products']['product']['eid']).to eq('50')
+        data = described_class.find_by_eid(50)
+        expect(data['eid']).to eq('50')
       end
     end
   end
